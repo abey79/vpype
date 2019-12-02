2d4bd6777281e62a166e025035e76fb3b492a2fe
Antoine Beyeler <abeyeler@ab-ware.com>
2019-12-02 21:52:31 +0100

    Fixed missing comma

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(exclude=("examples", "tests")),
     install_requires=[
         'Click',
-        'click-plugins'
+        'click-plugins',
         'matplotlib',
         'opencv-python',
         'rtree',
