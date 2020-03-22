c089b5330738b2b9bb5c92bdaefae3ea9c229d14
Antoine Beyeler <abeyeler@ab-ware.com>
2020-03-22 18:21:06 +0100

    Added legend to `show`
    The legend can be hidden with `--hide-legend`. Clicking on legend's lines toggle the corresponding layer's visibility.

--- vpype_cli/show.py
+++ vpype_cli/show.py
@@ -21,6 +21,7 @@ COLORS = [
 @click.option("-a", "--show-axes", is_flag=True, help="Display axes.")
 @click.option("-g", "--show-grid", is_flag=True, help="Display grid (implies -a).")
 @click.option("-p", "--show-pen-up", is_flag=True, help="Show pen-up trajectories.")
+@click.option("-h", "--hide-legend", is_flag=True, help="Do not display the legend.")
 @click.option(
     "-c", "--colorful", is_flag=True, help="Display each segment in a different color."
 )
@@ -37,6 +38,7 @@ def show(
     show_axes: bool,
     show_grid: bool,
     show_pen_up: bool,
+    hide_legend: bool,
     colorful: bool,
     unit: str,
 ):
@@ -50,9 +52,10 @@ def show(
 
     scale = 1 / convert(unit)
 
-    plt.figure()
+    fig = plt.figure()
     color_idx = 0
-    for lc in vector_data.layers.values():
+    collections = {}
+    for layer_id, lc in vector_data.layers.items():
         if colorful:
             color = COLORS[color_idx:] + COLORS[:color_idx]
             color_idx += len(lc)
@@ -62,11 +65,14 @@ def show(
         if color_idx >= len(COLORS):
             color_idx = color_idx % len(COLORS)
 
-        plt.gca().add_collection(
-            matplotlib.collections.LineCollection(
-                (as_vector(line) * scale for line in lc), color=color, lw=1, alpha=0.5
-            )
+        collections[layer_id] = matplotlib.collections.LineCollection(
+            (as_vector(line) * scale for line in lc),
+            color=color,
+            lw=1,
+            alpha=0.5,
+            label=str(layer_id),
         )
+        plt.gca().add_collection(collections[layer_id])
 
         if show_pen_up:
             plt.gca().add_collection(
@@ -83,6 +89,31 @@ def show(
 
     plt.gca().invert_yaxis()
     plt.axis("equal")
+
+    if not hide_legend:
+        lgd = plt.legend()
+        # we will set up a dict mapping legend line to orig line, and enable
+        # picking on the legend line
+        line_dict = {}
+        for lgd_line, lgd_text in zip(lgd.get_lines(), lgd.get_texts()):
+            lgd_line.set_picker(5)  # 5 pts tolerance
+            layer_id = int(lgd_text.get_text())
+            if layer_id in collections:
+                line_dict[lgd_line] = collections[layer_id]
+
+        def on_pick(event):
+            line = event.artist
+            vis = not line_dict[line].get_visible()
+            line_dict[line].set_visible(vis)
+
+            if vis:
+                line.set_alpha(1.0)
+            else:
+                line.set_alpha(0.2)
+            fig.canvas.draw()
+
+        fig.canvas.mpl_connect("pick_event", on_pick)
+
     if show_axes or show_grid:
         plt.axis("on")
         plt.xlabel(f"[{unit}]")
