ff2992dc2a611961ec40537066e35be749d43b08
Antoine Beyeler <abeyeler@ab-ware.com>
2020-08-16 21:54:20 +0200

    Closed paths are now properly closed in SVG

--- vpype/io.py
+++ vpype/io.py
@@ -273,9 +273,17 @@ def _line_to_path(dwg: svgwrite.Drawing, lines: Union[np.ndarray, LineCollection
     if isinstance(lines, np.ndarray):
         lines = [lines]
 
-    return dwg.path(
-        " ".join(("M" + " L".join(f"{x},{y}" for x, y in as_vector(line))) for line in lines),
-    )
+    def single_line_to_path(line: np.ndarray) -> str:
+        if line[0] == line[-1]:
+            closed = True
+            line = line[:-1]
+        else:
+            closed = False
+        return (
+            "M" + " L".join(f"{x},{y}" for x, y in as_vector(line)) + (" Z" if closed else "")
+        )
+
+    return dwg.path(" ".join(single_line_to_path(line) for line in lines))
 
 
 def write_svg(
