99649a11bc6fba35a3e3dcc6c25564559d768087
Antoine Beyeler <abeyeler@ab-ware.com>
2021-01-24 00:15:14 +0100

    - Added missing files in installer - updated changelog

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,6 +3,8 @@
 pyinstaller --noconfirm --onedir ^
     --name vpype --icon="scripts/vpype.ico" ^
     --add-data="vpype/hpgl_devices.toml;vpype" ^
+    --add-data="vpype_viewer/shaders;vpype_viewer" ^
+    --add-data="vpype_viewer/qtviewer/resources;vpype_viewer/qtviewer" ^
     --additional-hooks-dir=scripts/hooks ^
     scripts/run_vpype.py
 
