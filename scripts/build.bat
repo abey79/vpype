9d79cafc9cd334bf521d425350440f63290ded19
Antoine Beyeler <abeyeler@ab-ware.com>
2021-01-24 12:43:49 +0100

    Fixed installer script

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,9 +3,9 @@
 pyinstaller --noconfirm --onedir ^
     --name vpype --icon="scripts/vpype.ico" ^
     --add-data="vpype/hpgl_devices.toml;vpype" ^
-    --add-data="vpype_viewer/shaders;vpype_viewer" ^
-    --add-data="vpype_viewer/qtviewer/resources;vpype_viewer/qtviewer" ^
+    --add-data="vpype_viewer/shaders;vpype_viewer/shaders" ^
+    --add-data="vpype_viewer/qtviewer/resources;vpype_viewer/qtviewer/resources" ^
     --additional-hooks-dir=scripts/hooks ^
     scripts/run_vpype.py
 
-makensis /V4 /DVERSION="%1" scripts/installer_win.nsi
+@Rem makensis /V4 /DVERSION="%1" scripts/installer_win.nsi
