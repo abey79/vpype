abey79/vpype

commit 7d9395259b4dced4dcf37cc8b3aeeb9f4e39aed5

Add continuous benchmarking setup (#184)
The following is tested:
- read_svg
- linemerge/linesort
- viewer engine

Also added deferring of _build in Engine, for a very significant speedup

vpype_viewer/engine.py
@@ -86,6 +86,7 @@ class Engine:
         self._scale = 1.0  # one pixel of page equal one pixel of view port
         self._origin = (0.0, 0.0)  # top-left of page aligned with top-left of view port
         self._document: Optional[vp.Document] = None
+        self._rebuild_needed = True
 
         # painters
         self._layer_visibility: Dict[int, bool] = defaultdict(lambda: True)
@@ -99,7 +100,6 @@ class Engine:
         self._ctx.enable_only(mgl.BLEND | mgl.PROGRAM_POINT_SIZE)
 
         self.resize(width, height)
-        self._rebuild()
 
     # =========================================================================================
     # Properties
@@ -113,7 +113,7 @@ class Engine:
     def document(self, document: Optional[vp.Document]) -> None:
         self._document = document
         self._layer_visibility.clear()
-        self._rebuild()
+        self._rebuild_needed = True
 
     @property
     def scale(self) -> float:
@@ -143,7 +143,7 @@ class Engine:
     @view_mode.setter
     def view_mode(self, view_mode: ViewMode) -> None:
         self._view_mode = view_mode
-        self._rebuild()
+        self._rebuild_needed = True
 
     @property
     def show_pen_up(self) -> bool:
@@ -153,7 +153,7 @@ class Engine:
     @show_pen_up.setter
     def show_pen_up(self, show_pen_up: bool):
         self._show_pen_up = show_pen_up
-        self._rebuild()
+        self._rebuild_needed = True
 
     @property
     def show_points(self) -> bool:
@@ -163,7 +163,7 @@ class Engine:
     @show_points.setter
     def show_points(self, show_points: bool):
         self._show_points = show_points
-        self._rebuild()
+        self._rebuild_needed = True
 
     @property
     def pen_width(self) -> float:
@@ -173,7 +173,7 @@ class Engine:
     @pen_width.setter
     def pen_width(self, pen_width: float):
         self._pen_width = pen_width
-        self._rebuild()
+        self._rebuild_needed = True
 
     @property
     def pen_opacity(self) -> float:
@@ -183,7 +183,7 @@ class Engine:
     @pen_opacity.setter
     def pen_opacity(self, pen_opacity: float):
         self._pen_opacity = pen_opacity
-        self._rebuild()
+        self._rebuild_needed = True
 
     @property
     def debug(self) -> bool:
@@ -305,6 +305,9 @@ class Engine:
         if self._ctx is None:
             return
 
+        if self._rebuild_needed:
+            self._rebuild()
+
         self._ctx.clear(0.95, 0.95, 0.95, 1)
         proj = self._get_projection()
 
@@ -371,4 +374,5 @@ class Engine:
                     self._ctx, self._document.page_size
                 )
 
+        self._rebuild_needed = False
         self._render_cb()

.github/workflows/python-lint-tests.yml
@@ -87,14 +87,16 @@ jobs:
     # PYTEST STRATEGY
     # macOS is the only runner who has working ModernGL behaviour
     # macOS + 3.8 is used for code coverage
-    - name: Pytest (code coverage)
-      run: poetry run pytest --mpl --mpl-baseline-path tests/data/baseline --cov=./ --cov-report=xml
+    - name: Pytest (code coverage + benchmark)
+      run: |
+        poetry run pytest --cov=./ --cov-report=xml
+        poetry run pytest tests/test_benchmarks.py --benchmark-enable --benchmark-json output.json
       if: matrix.os == 'macos-latest' && matrix.python-version == '3.8'
     - name: Pytest
-      run: poetry run pytest --mpl --mpl-baseline-path tests/data/baseline
+      run: poetry run pytest
       if: matrix.os == 'macos-latest' && matrix.python-version != '3.8'
     - name: Pytest (no image similarity check)
-      run: poetry run pytest --mpl --mpl-baseline-path tests/data/baseline --skip-image-similarity
+      run: poetry run pytest --skip-image-similarity
       if: matrix.os != 'macos-latest'
     - name: Upload test results
       if: always()
@@ -108,6 +110,20 @@ jobs:
         token: ${{ secrets.CODECOV_TOKEN }}
         file: ./coverage.xml
       if: matrix.os == 'macos-latest' && matrix.python-version == '3.8'
+    - name: Store benchmark result
+      uses: rhysd/github-action-benchmark@v1
+      if: matrix.os == 'macos-latest' && matrix.python-version == '3.8'
+      with:
+        tool: 'pytest'
+        output-file-path: output.json
+        # push to gh-page
+        github-token: ${{ secrets.PERSONAL_GITHUB_TOKEN }}
+        auto-push: true
+        # Show alert with commit comment on detecting possible performance regression
+        alert-threshold: '150%'
+        comment-on-alert: true
+        fail-on-alert: true
+
 
   #################
   # TESTS WINDOWS #
@@ -138,7 +154,7 @@ jobs:
       run: poetry install
     - name: Pytest
       run: |
-        poetry run pytest --mpl --mpl-baseline-path tests/data/baseline --skip-image-similarity
+        poetry run pytest --skip-image-similarity
     - name: Upload test results
       if: always()
       uses: actions/upload-artifact@v2

pyproject.toml
@@ -60,6 +60,7 @@ recommonmark = { version = ">=0.6,<0.8", optional = true }
 coverage = {extras = ["toml"], version = "^5.4"}
 pytest = "^6.2.1"
 pytest-cov = "^2.11.0"
+pytest-benchmark = "^3.2.3"
 black = "^20.8b1"
 isort = "^5.7.0"
 mypy = "^0.800"
@@ -74,6 +75,8 @@ docs = ["Sphinx", "sphinx-click", "sphinx-autodoc-typehints", "sphinx-rtd-theme"
 requires = ["poetry-core>=1.0.0"]
 build-backend = "poetry.core.masonry.api"
 
+[tool.pytest.ini_options]
+addopts = "--mpl --mpl-baseline-path tests/data/baseline --benchmark-disable"
 
 [tool.coverage.run]
 branch = true

commit a316c8aadeb8c2a4e014af634be4fb96cb959a76

Fixed updating logic problems introduced in #184

vpype_viewer/engine.py
@@ -113,7 +113,7 @@ class Engine:
     def document(self, document: Optional[vp.Document]) -> None:
         self._document = document
         self._layer_visibility.clear()
-        self._rebuild_needed = True
+        self._update()
 
     @property
     def scale(self) -> float:
@@ -123,7 +123,7 @@ class Engine:
     @scale.setter
     def scale(self, scale: float) -> None:
         self._scale = scale
-        self._render_cb()
+        self._update(False)
 
     @property
     def origin(self) -> Tuple[float, float]:
@@ -143,7 +143,7 @@ class Engine:
     @view_mode.setter
     def view_mode(self, view_mode: ViewMode) -> None:
         self._view_mode = view_mode
-        self._rebuild_needed = True
+        self._update()
 
     @property
     def show_pen_up(self) -> bool:
@@ -153,7 +153,7 @@ class Engine:
     @show_pen_up.setter
     def show_pen_up(self, show_pen_up: bool):
         self._show_pen_up = show_pen_up
-        self._rebuild_needed = True
+        self._update()
 
     @property
     def show_points(self) -> bool:
@@ -163,7 +163,7 @@ class Engine:
     @show_points.setter
     def show_points(self, show_points: bool):
         self._show_points = show_points
-        self._rebuild_needed = True
+        self._update()
 
     @property
     def pen_width(self) -> float:
@@ -173,7 +173,7 @@ class Engine:
     @pen_width.setter
     def pen_width(self, pen_width: float):
         self._pen_width = pen_width
-        self._rebuild_needed = True
+        self._update()
 
     @property
     def pen_opacity(self) -> float:
@@ -183,7 +183,7 @@ class Engine:
     @pen_opacity.setter
     def pen_opacity(self, pen_opacity: float):
         self._pen_opacity = pen_opacity
-        self._rebuild_needed = True
+        self._update()
 
     @property
     def debug(self) -> bool:
@@ -193,7 +193,7 @@ class Engine:
     @debug.setter
     def debug(self, debug: bool):
         self._debug = debug
-        self._render_cb()
+        self._update(False)
 
     def layer_visible(self, layer_id: int) -> bool:
         """True if the corresponding layer is currently visible.
@@ -210,7 +210,7 @@ class Engine:
             layer_id: layer to toggle
         """
         self._layer_visibility[layer_id] = not self._layer_visibility[layer_id]
-        self._render_cb()
+        self._update(False)
 
     # =========================================================================================
     # Geometry
@@ -239,7 +239,7 @@ class Engine:
             y1 - (self._viewport_height / self._scale - h) / 2,
         ]
 
-        self._render_cb()
+        self._update(False)
 
     def viewport_to_model(self, x: float, y: float) -> Tuple[float, float]:
         """Converts viewport coordinates to model coordinates."""
@@ -275,7 +275,7 @@ class Engine:
             dy: vertical distance to pan
         """
         self._origin = (self._origin[0] - dx / self._scale, self._origin[1] - dy / self._scale)
-        self._render_cb()
+        self._update(False)
 
     def zoom(self, delta_zoom: float, mouse_x: float, mouse_y: float) -> None:
         """Zoom the viewport.
@@ -295,7 +295,7 @@ class Engine:
         self._origin = (self._origin[0] + mouse_x * dz, self._origin[1] + mouse_y * dz)
         self._scale = new_scale
 
-        self._render_cb()
+        self._update(False)
 
     # =========================================================================================
     # Painters
@@ -318,6 +318,12 @@ class Engine:
             if self._layer_visibility[layer_id]:
                 for painter in self._layer_painters[layer_id]:
                     painter.render(proj, self._scale, self._debug)
+
+    def _update(self, rebuild=True):
+        """"""
+        if rebuild:
+            self._rebuild_needed = True
+        self._render_cb()
 
     def _rebuild(self):
         self._layer_painters.clear()
@@ -375,4 +381,3 @@ class Engine:
                 )
 
         self._rebuild_needed = False
-        self._render_cb()