bf67511c6b4f940c028f60e53dfba230a80dbcc1
Antoine Beyeler <abeyeler@ab-ware.com>
2022-01-31 18:10:09 +0100

    Cleaned up _BrokenCommand

--- vpype_cli/cli.py
+++ vpype_cli/cli.py
@@ -77,7 +77,7 @@ class GroupedGroup(click.Group):
         return super().main(args=preprocess_argument_list(args), **extra)
 
 
-class _BrokenCommand(click.Command):
+class _BrokenCommand(click.Command):  # pragma: no cover
     """Rather than completely crash the CLI when a broken plugin is loaded, this
     class provides a modified help message informing the user that the plugin is
     broken and they should contact the owner.  If the user executes the plugin
@@ -86,35 +86,21 @@ class _BrokenCommand(click.Command):
     """
 
     def __init__(self, name):
-
-        """
-        Define the special help messages after instantiating a `click.Command()`.
-        """
+        """Define the special help messages after instantiating a `click.Command()`."""
 
         click.Command.__init__(self, name)
-
         util_name = os.path.basename(sys.argv and sys.argv[0] or __file__)
-
-        if os.environ.get("CLICK_PLUGINS_HONESTLY"):  # pragma no cover
-            icon = "\U0001F4A9"
-        else:
-            icon = "\u2020"
-
         self.help = (
             "\nWarning: entry point could not be loaded. Contact "
             "its author for help.\n\n\b\n" + traceback.format_exc()
         )
-        self.short_help = icon + " Warning: could not load plugin. See `%s %s --help`." % (
+        self.short_help = "\u2020 Warning: could not load plugin. See `%s %s --help`." % (
             util_name,
             self.name,
         )
 
     def invoke(self, ctx):
-
-        """
-        Print the traceback instead of doing nothing.
-        """
-
+        """Print the traceback instead of doing nothing."""
         click.echo(self.help, color=ctx.color)
         ctx.exit(1)
 
