e15b76a045618c56fdeb3984546ea54d8bafc100
Antoine Beyeler <abeyeler@ab-ware.com>
2022-02-14 15:35:25 +0100

    - added ref SVG test infrastructure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+import contextlib
+import difflib
+import hashlib
 import os
 import pathlib
-from typing import Callable
+from typing import Callable, List
 
 import numpy as np
 import pytest
@@ -54,6 +57,12 @@ def pytest_addoption(parser):
         "--skip-image-similarity",
         action="store_true",
         help="Skip tests using assert_image_similarity().",
+    )
+
+    parser.addoption(
+        "--store-ref-svg",
+        action="store_true",
+        help="Write reference SVGs for reference_svg().",
     )
 
 
@@ -118,3 +127,59 @@ def assert_image_similarity(request) -> Callable:
                     )
 
     return _assert_image_similarity
+
+
+def _read_SVG_lines(path: pathlib.Path) -> List[str]:
+    return [
+        line
+        for line in path.read_text().splitlines()
+        if "<dc:source>" not in line and "<dc:date>" not in line
+    ]
+
+
+@pytest.fixture
+def reference_svg(request, tmp_path) -> Callable:
+    """Compare a SVG output to a saved reference.
+
+    Use `--store-ref-svg` to save reference SVGs.
+
+    Example::
+
+        def test_ref_svg(reference_svg):
+            with reference_svg() as path:
+                export_svg_to(path)
+    """
+    store_ref_svg = request.config.getoption("--store-ref-svg")
+    test_id = "refsvg_" + hashlib.md5(request.node.name.encode()).hexdigest() + ".svg"
+    ref_path = pathlib.Path(REFERENCE_IMAGES_DIR) / test_id
+    temp_file = tmp_path / test_id
+
+    @contextlib.contextmanager
+    def _reference_svg():
+        nonlocal ref_path, temp_file, store_ref_svg
+
+        yield temp_file
+
+        if store_ref_svg:
+            ref_path.write_bytes(temp_file.read_bytes())
+        else:
+            if not ref_path.exists():
+                pytest.fail(f"reference SVG does not exist")
+
+            temp_lines = _read_SVG_lines(temp_file)
+            ref_lines = _read_SVG_lines(ref_path)
+
+            if len(temp_lines) != len(ref_lines) or not all(
+                a == b for a, b in zip(temp_lines, ref_lines)
+            ):
+                delta = difflib.unified_diff(
+                    temp_lines,
+                    ref_lines,
+                    fromfile="<test result>",
+                    tofile=str(ref_path.relative_to(pathlib.Path(REFERENCE_IMAGES_DIR))),
+                    lineterm="",
+                )
+
+                pytest.fail(f"generated SVG does not match reference:\n" + "\n".join(delta))
+
+    return _reference_svg
---
+++ tests/test_reference_output.py
@@ -0,0 +1,17 @@
+import pytest
+
+import vpype_cli
+
+
+@pytest.mark.parametrize(
+    "cmd",
+    [
+        "line 0 0 10 10 write {path}",
+        """pens cmyk grid -o 10cm 10cm 2 2 rect -l new 1cm 1cm 8cm 8cm end
+        layout a4 write {path}""",
+    ],
+)
+def test_reference_output(reference_svg, runner, cmd):
+    with reference_svg() as path:
+        res = runner.invoke(vpype_cli.cli, cmd.format(path=path))
+        assert res.exit_code == 0
