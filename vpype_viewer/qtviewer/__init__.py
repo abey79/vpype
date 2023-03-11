a045357b0581a519f2b3c385a824cbdfa7c3411e
Antoine Beyeler <abeyeler@ab-ware.com>
2023-03-11 18:51:32 +0100

    Fixed Wayland-related crash by setting QT_QPA_PLATFORM to "xcb"
    Fixes #596
    
    [skip ci]

--- vpype_viewer/qtviewer/__init__.py
+++ vpype_viewer/qtviewer/__init__.py
@@ -1,1 +1,18 @@
+def _check_wayland():
+    """Fix QT env variable on Wayland-based systems.
+
+    See https://github.com/abey79/vpype/issues/596
+    """
+    import os
+    import sys
+
+    if sys.platform.startswith("linux"):
+        if os.environ.get("XDG_SESSION_TYPE", "") == "wayland":
+            if "QT_QPA_PLATFORM" not in os.environ:
+                os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+
+_check_wayland()
+
+
 from .viewer import *
