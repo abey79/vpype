b8d81954064a4ffc50af1d21ad19ebb58b6d344e
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2024-01-07 10:37:22 +0100

    Update dependencies (#677)

--- vpype_cli/show.py
+++ vpype_cli/show.py
@@ -175,7 +175,10 @@ def _show_mpl(
 
     fig = plt.figure()
     color_idx = 0
-    collections = {}
+    collections: dict[
+        int,
+        list[matplotlib.collections.LineCollection | matplotlib.collections.PathCollection],
+    ] = {}
 
     # draw page boundaries
     if document.page_size:
@@ -210,7 +213,7 @@ def _show_mpl(
             color_idx = color_idx % len(COLORS)
 
         layer_lines = matplotlib.collections.LineCollection(
-            (vp.as_vector(line) * scale for line in lc),
+            [vp.as_vector(line) * scale for line in lc],
             color=color,
             lw=1,
             alpha=0.5,
@@ -228,10 +231,10 @@ def _show_mpl(
 
         if show_pen_up:
             pen_up_lines = matplotlib.collections.LineCollection(
-                (
+                [
                     (vp.as_vector(lc[i])[-1] * scale, vp.as_vector(lc[i + 1])[0] * scale)
                     for i in range(len(lc) - 1)
-                ),
+                ],
                 color=(0, 0, 0),
                 lw=0.5,
                 alpha=0.5,
@@ -276,5 +279,5 @@ def _show_mpl(
     else:
         plt.axis("off")
     if show_grid:
-        plt.grid("on")
+        plt.grid(True)
     plt.show()
